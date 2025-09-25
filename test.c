f05f6e012a1af947d69bbe47cbfbc27c5faf1838
Jeremy Lorelli <lorelli@slac.stanford.edu>
2025-09-24 18:03:26 -0700

    add expand_macro_string

---
+++ test.c
@@ -0,0 +1,17 @@
+
+#include "strtools.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int
+main(int argc, char** argv)
+{
+  for (int i = 1; i < argc; ++i) {
+    char* s = expand_macro_string_env(argv[i]);
+    puts(s);
+    free(s);
+  }
+  return 0;
+}
